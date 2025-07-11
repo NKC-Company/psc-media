9a1c0428e488907b55835906d8ff2adbd4cb8acd
ThanhTrong29 <213574275+Dev-NKC@users.noreply.github.com>
2025-07-11 17:16:48 +0700

    initial

---
+++ staffList.js
@@ -0,0 +1,23 @@
+const staffList = [
+  {
+    name: "Nguyễn Thanh Trọng",
+    title: "NKCentral Group",
+    phone: "0338312415",
+    email: "trong.nguyen@nkcmc.com.vn",
+    website: "https://nkcmc.com.vn"
+  },
+  {
+    name: "Chiến Ngô",
+    title: "App Operation Specialist - NKCentral Group",
+    phone: "0938467466",
+    email: "chien.ngo@nkmc.com.vn",
+    website: "https://nkcmc.com.vn , https://galaxy365.vn"
+  },
+  {
+    name: "Lê Viết Long",
+    title: "Nhân viên - NKCentral Group",
+    phone: "0708695592",
+    email: "long.le@hcmute.edu.vn",
+    website: "https://nkcmc.com.vn"
+  }
+];
